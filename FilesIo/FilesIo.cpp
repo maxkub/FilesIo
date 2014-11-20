94c1f01853db77f8853135ffbbc67195d227935a
maxkub <maxime.kubryk@gmail.com>
2014-11-20 01:10:58 +0100

    import_multi

--- FilesIo/FilesIo.cpp
+++ FilesIo/FilesIo.cpp
@@ -11,9 +11,8 @@ namespace FilesIo
 {
 
 	// read function
-	vector<vector<double>> import(string path, int skip, bool onscreen)
+	vector<vector<double>> import(string path, int skip, char delim, bool onscreen)
 	{
-
 		vector<vector<double>> arr = {};
 		vector<double> line = {};
 
@@ -36,8 +35,17 @@ namespace FilesIo
 				stringstream ss(input);
 				line.clear();
 
-				while (ss >> x)
+				//while (ss >> x)
+				//{
+				//	line.push_back(x);
+				//}
+
+				string field;
+				while (getline(ss, field, delim))
 				{
+					stringstream fs(field);
+
+					fs >> x;					
 					line.push_back(x);
 				}
 
@@ -60,6 +68,90 @@ namespace FilesIo
 			exit(1);
 		}
 
+
+		return arr;
+	}
+
+
+
+
+
+
+
+
+	// read multiple types
+	vector<vector<typeA>> import_multi(string path, int skip, char delim, bool onscreen)
+	{
+
+		vector<vector<typeA>> arr = {};
+		vector<typeA> line = {};
+
+		ifstream file(path.c_str());
+
+		if (file)
+		{
+			typeA x;
+			string input;
+
+			// Skip rows
+			for (int i = 0; i < skip; ++i)
+			{
+				getline(file, input);
+			}
+
+			// read lines and build the output array
+			while (getline(file, input))
+			{
+				stringstream ss(input);
+				line.clear();
+
+				string field;
+				while (getline(ss, field, delim))
+				{
+					stringstream fs(field);
+
+					try
+					{
+						fs >> x.I;
+						x.type = 1;
+					}
+					catch (...){}
+
+					try
+					{
+						fs >> x.D;
+						x.type = 2;
+					}
+					catch (...){}
+
+					try
+					{
+						fs >> x.S;
+						x.type = 3;
+					}
+					catch (...){}
+
+					line.push_back(x);
+				}
+
+				// print curent line on screen
+				if (onscreen)
+				{
+					for (size_t i = 0; i < line.size(); ++i)
+					{
+						cout << line[i] << " ";
+					}
+					cout << endl;
+				}
+
+				arr.push_back(line);
+			}
+		}
+		else
+		{
+			cout << "ERROR in files_io.read : can't open file : " << path.c_str() << endl;
+			exit(1);
+		}
 
 		return arr;
 	}
--- FilesIo/FilesIo.h
+++ FilesIo/FilesIo.h
@@ -6,13 +6,50 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+//#include <ostream>
 
 namespace FilesIo
 {
 	
+	//data types
+	__declspec(dllexport) struct typeA
+	{
+		int I;
+		double D;
+		std::string S;
+
+		int type;
+
+		friend std::ostream& operator <<(std::ostream& os, typeA const& a)
+		{
+			switch (a.type)
+			{
+			case 1:
+				return os << a.I << "\n";
+				break;
+			case 2:
+				return os << a.D << "\n";
+				break;
+			case 3:
+				return os << a.S << "\n";
+				break;
+			default:
+				return os << "..." << "\n";
+				break;
+			}
+			
+		}
+
+	};
+
+
+
+	
 
 	// read function
-	__declspec(dllexport) std::vector<std::vector<double>> import(std::string path, int skip = 0, bool onscreen = false);
+	__declspec(dllexport) std::vector<std::vector<double>> import(std::string path, int skip = 0, char delim = ' ', bool onscreen = false);
+
+	__declspec(dllexport) std::vector<std::vector<typeA>> import_multi(std::string path, int skip = 0, char delim = ' ', bool onscreen = false);
 
 	// write function
 	__declspec(dllexport) std::vector<std::vector<double>> write(std::string path);
